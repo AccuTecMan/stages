98daf8143ed5d9a5a648370b390a40d97047575c
Alonso <alonso.gamez@gmail.com>
2023-11-15 21:56:58 -0600

    Setting the layout for the login page

--- src/app/common/app-common.module.ts
+++ src/app/common/app-common.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 import { HeaderComponent } from './components/header.component';
 
 @NgModule({
