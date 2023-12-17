74772da051c5f3a0b0e69ff4b3fd2506ae4a68ea
Gámez <AGamez@bethel.jw.org>
2023-12-17 15:49:15 -0600

    Adjust store to have Search Criteria

--- src/app/features/cutting-sheets/store/selectors/selectors.ts
+++ src/app/features/cutting-sheets/store/selectors/selectors.ts
@@ -1,14 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-import { featureName, CuttingSheersState, cuttingSheetsAdapter } from '../reducers';
+import { featureName, CuttingSheetsState, cuttingSheetsAdapter } from '../reducers';
 import { EntityLoadStatus } from '@core/models';
 
-export const selectCuttingSheetsState = createFeatureSelector<CuttingSheersState>(featureName);
+export const selectCuttingSheetsState = createFeatureSelector<CuttingSheetsState>(featureName);
 
 export const selectCuttingSheetsLoadStatus = createSelector(selectCuttingSheetsState, (state) => state.status);
 
 export const selectIsLoading = createSelector(selectCuttingSheetsState, (state) => state.status === EntityLoadStatus.LOADING);
 
-export const cuttingSheetsSelectors = cuttingSheetsAdapter.getSelectors(selectCuttingSheetsState);
-
+export const selectSummaryState = createSelector(selectCuttingSheetsState, (state) => state.summary);
+export const cuttingSheetsSelectors = cuttingSheetsAdapter.getSelectors(selectSummaryState);
 export const selectAllCuttingSheets = createSelector(cuttingSheetsSelectors.selectAll, (cus) => cus);
