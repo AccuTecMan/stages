a5ccc857f7ac81aef0441d035394737c21c6a040
Alonso <alonso.gamez@gmail.com>
2024-01-13 11:59:28 -0600

    Adding stages list to the component

--- src/app/base/store/selectors/stage-templates.selectors.ts
+++ src/app/base/store/selectors/stage-templates.selectors.ts
@@ -1,6 +1,7 @@
 import { createSelector } from "@ngrx/store";
 import { selectCuttingSheetsState } from './feature.selectors';
 import { CuttingSheetsState } from "../reducers";
+import { StageMap } from "@app/base/models";
 
 export const selectStageTemplatesLoadedStatus = createSelector(
   selectCuttingSheetsState,
@@ -8,3 +9,10 @@ export const selectStageTemplatesLoadedStatus = createSelector(
 
   export const selectAllTemplates = createSelector(
     selectCuttingSheetsState, (state: CuttingSheetsState) => state.stageTemplates.stageTemplates);
+
+  export const selectAllStageMap = createSelector(
+    selectCuttingSheetsState, (state: CuttingSheetsState) =>{
+      let ordered = state.stagesMap.stagesMap?.slice().sort((a, b) =>  (a.name < b.name ? -1 : 1));
+      ordered?.unshift(<StageMap>{ id: '0', name: 'All' });
+      return ordered;
+  })
